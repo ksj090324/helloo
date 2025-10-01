343a8c4aa202a25c97bcd1a66edfd20083aab6c1
ksj090324 <132073849+ksj090324@users.noreply.github.com>
2025-10-01 09:10:46 +0000

    첫번쨰 앱 배포

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.success("hi")
+st.info("hi")
+st.image("https://i.namu.wiki/i/_poWKL32BWBvnSSEwoPtCsYdRdxUrsytNk2bdcAS4L2bXzU3nAvtvGKim_ai90tttpmNxjACyNLnMWpHkXNDrQ.webp")
