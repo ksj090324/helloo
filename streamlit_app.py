2dbe2819b7ee41317a189f42ccd8002363adc464
ksj090324 <132073849+ksj090324@users.noreply.github.com>
2025-10-01 09:20:42 +0000

    첫번쨰 앱 배포

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 서진의 첫번째 앱")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "하이"
 )
 st.success("hi")
 st.info("hi")
